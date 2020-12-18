22e10f9c6109a55db15d79c160e6964aeaf74ab3
Mihkel Kougia <kougia.mihkel@khk.ee>
2020-12-18 19:05:51 +0200

    Teade väljastamine Java abil

--- src/test.java
+++ src/test.java
@@ -1,6 +1,9 @@
 public class test {
     public static void main(String[] args) {
-        System.out.println("Tere, Java programm!");
-        System.out.println("See on java IDE katse");
+        System.out.println("Täna õppisime:");
+        System.out.println("1. JDK installeerimine ja seadistamine");
+        System.out.println("2. IDE installeerimine ja seadistamine");
+        System.out.println("3. IDE kasutamine");
+        System.out.println("4. git VCS kasutamine");
     }
 }
