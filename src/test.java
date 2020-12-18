2f725ab684b76016bc66c5b9f524a2c39ad05921
Mihkel Kougia <kougia.mihkel@khk.ee>
2020-12-18 19:01:45 +0200

    Esimene java programm on loodud

---
+++ src/test.java
@@ -0,0 +1,6 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Tere, Java programm!");
+        System.out.println("See on java IDE katse");
+    }
+}
