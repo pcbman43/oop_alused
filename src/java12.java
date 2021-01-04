429b51f33e68d3543363063a66ca864df3b911ca
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-01-04 20:11:59 +0200

    Muutujad ja andmetüübid - ülesanne 1.2

---
+++ src/java12.java
@@ -0,0 +1,13 @@
+/*
+ * ülesanne 1.2
+ */
+
+public class java12 {
+    public static void main(String[] args) {
+        int aasta = 2020;
+        String liblikas = "teelehe-mosaaikliblikas";
+        String lauseKeskosa = ". aasta liblikas on ";
+        String lause = aasta + lauseKeskosa + liblikas;
+        System.out.println(lause);
+    }
+}
