7e986b1f94026119dfaeb0bf8c3f621f589e2fb6
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-01-04 21:33:40 +0200

    Tavaline tingimuslause (if-else) - 2.1

--- src/test.java
+++ src/test.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 /*
  * Muutujate kasutamise näide
  * autor - Mihkel Kougia
- * ülesanne 1.3
+ * ülesanne 2.1
  */
 public class test {
     public static void main(String[] args) {
-
         Scanner sisend = new Scanner(System.in);
-        System.out.println("Sisestage astme alus: ");
-        int astmeAlus = sisend.nextInt();
-        System.out.println("Sisestage astendaja: ");
-        int astendaja = sisend.nextInt();
+        System.out.println("Sisestage pilede aluse kõrgus (km): ");
+        double pilved = sisend.nextDouble();
 
-        int tulemus = (int) Math.pow(astmeAlus, astendaja);
-
-        System.out.println(tulemus);
+        if (pilved > 6.0) {
+            System.out.println("Need on ülemised pilved.");
+        } else {
+            System.out.println("Need ei ole ülemised pilved.");
+        }
     }
 }
