0b573749b728f2a6f7b25505f697709f16438b4e
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-01-04 20:55:37 +0200

    Kasutaja sisend ning tüübiteisendus - ülesanne 1.3

--- src/test.java
+++ src/test.java
@@ -1,10 +1,21 @@
+import java.util.Scanner;
+
+/*
+ * Muutujate kasutamise näide
+ * autor - Mihkel Kougia
+ * ülesanne 1.3
+ */
 public class test {
     public static void main(String[] args) {
-        System.out.println("Tere, Maailm!");
-        int aasta = 2020;
-        String liblikas = "teelehe-mosaaikliblikas";
-        String lauseKeskosa = ". aasta liblikas on ";
-        String lause = aasta + lauseKeskosa + liblikas;
-        System.out.println(lause);
+
+        Scanner sisend = new Scanner(System.in);
+        System.out.println("Sisestage astme alus: ");
+        int astmeAlus = sisend.nextInt();
+        System.out.println("Sisestage astendaja: ");
+        int astendaja = sisend.nextInt();
+
+        int tulemus = (int) Math.pow(astmeAlus, astendaja);
+
+        System.out.println(tulemus);
     }
 }
