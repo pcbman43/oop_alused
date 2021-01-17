24e29ce20a726fdabe8627cbfbbb4d8201e77bfd
Mihkel Kougia <kougia.mihkel@khk.ee>
2021-01-17 16:12:13 +0200

    meetodid - ülesanne 6.2

--- src/test.java
+++ src/test.java
@@ -1,23 +1,22 @@
 import java.util.Scanner;
 
 /*
- * while tsükkel
+ * meetod + return operaator
  * autor - Mihkel Kougia
- * ülesanne 3.3
- */
+ * ülesanne 6.2
+ * */
 public class test {
+    static int mahlapakkideArv(double ounteKogus) {
+        int pakkideArv = (int) Math.round(ounteKogus * 0.4 / 3);
+        return pakkideArv;
+    }
+
     public static void main(String[] args) {
-        Scanner sisend = new Scanner(System.in);
-        System.out.print("Mitu pöialpoissi tahab õunu? ");
-        int mitu = sisend.nextInt();
-        int lumevalgekeseOunad = 14;
-        int poialpoiss = 1;
-        while (poialpoiss <= mitu) {
-            int ounad = (int) (Math.random() * 3);
-            System.out.println(ounad);
-            lumevalgekeseOunad -= ounad;
-            poialpoiss++;
-        }
-        System.out.println("Lumevalgekesele jäi " + lumevalgekeseOunad);
+        Scanner input = new Scanner(System.in);
+        System.out.println("Sisestage õunte kogus kilogrammides: ");
+        double mitu = input.nextDouble();
+
+        System.out.println(mahlapakkideArv(mitu));
+
     }
 }
